0542e2f5573fb260412fce34f684c33b953cbbba
Vixo341 <35233021+Vixo341@users.noreply.github.com>
2023-05-23 17:00:43 +0200

    entities and repositories for product

---
+++ Shopping/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product> CreateProduct(Product product);
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+
+
+    Task<Product> GetProduct(string id);
+
+    Task<IEnumerable<Product>> GetProducts();
+    Task<IEnumerable<Product>> GetProductByName(string name);
+    Task<IEnumerable<Product>> GetProductByBrand(string brand);
+
+
+}
